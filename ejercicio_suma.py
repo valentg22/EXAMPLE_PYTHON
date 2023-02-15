d8e8470a88d4d2b9e37a8907b83c3ebc7ace9252
valentg22 <vtobongarcia1023@gmail.com>
2023-02-15 11:19:52 -0500

    se creo comentario y la operacion restar

--- ejercicio_suma.py
+++ ejercicio_suma.py
@@ -1,6 +1,14 @@
 number_one = 6
-numbes_two = 3
-
+number_two = 5
+#this is a comment of one line
+#operacion de sumar los dos numeros
+"""
+This is a multi-line comment
 sumar = number_one + number_two
 
 print(sumar)
+
+"""
+
+resta = number_one - number_two
+print(resta)
