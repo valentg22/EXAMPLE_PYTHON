35066a3b20c3a829e582782643c21be4228bda1d
valentg22 <vtobongarcia1023@gmail.com>
2023-02-22 10:15:10 -0500

    almacen.py

---
+++ Almacen_WC.py
@@ -0,0 +1,32 @@
+articulos=["zapatos", "tenis", "camisetas", "jeans"]
+print (articulos)
+print(articulos[3])
+
+#Consola de articulos y precios actuales
+valor= ["zapatos, 350"], ["tenis, 280"], ["camisetas, 175"], ["jeans, 200"]
+print(valor)
+
+#costo total cuatro articulos
+price = [350, 280, 175, 200]
+resultado = price[0] + price[1] + price[2] + price[3]
+print (resultado)
+
+#promedio de venta
+price = [350, 280, 175, 200]
+resultado = price[0] + price[1] + price[2] + price[3]/4
+print (resultado)
+
+#subir precio de jeans
+resultado = (price [3] *0.062) + price [3]
+print (resultado)
+
+#disminuir precio zapatos
+resultado = (price [0] *0.008) - price [0]
+print (resultado)
+
+#nuevos valores
+Nuevo_precio_jeans = (price [3] *0.062) + price [3]
+print (Nuevo_precio_jeans)
+
+Nuevo_precio_zapatos = (price [0] *0.008) - price [0]
+print (Nuevo_precio_zapatos)
