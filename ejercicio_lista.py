52ada7619e5563be44f62f5af6c292b358f2e204
valentg22 <vtobongarcia1023@gmail.com>
2023-02-22 07:37:29 -0500

    ejercicio_list

---
+++ ejercicio_lista.py
@@ -0,0 +1,26 @@
+#examples of list
+#nombres = ["Jesus", "Maria", "Jose"]
+#print(nombres)
+#print(nombres[1])
+
+#datos_persona ["Jesus", 33, 1.75, True] en forma de lista
+#en forma de Diccionario
+
+"""
+datos_persona = {
+"nombre":"Jesus", 
+"edad": 33, 
+"estatura": 1.75, 
+"soltero?": True
+}
+"""
+
+#En forma de tupla
+datos_persona ("Jesus", 33, 1.75, True)
+#print(datos_persona[2])
+
+price = [100, 250, 300]
+resultado = price[0] + price[2]
+print (resultado)
+print("El cliente", datos_persona[0], "compro $", resultado)
+print(price)
